29a51e19602a15658635a7fe407781d73fcf7aa5
Nicola Prevedello <n.prevedello@reply.it>
2023-03-05 10:56:53 +0100

    Aggiunti commenti al codice.

--- 2022/Pandora's Adventures.py
+++ 2022/Pandora's Adventures.py
@@ -12,10 +12,11 @@ dataSet5 = "05-androids-armageddon"
 
 dataSetInTesting = dataSet0
 
+#Input Parser in base al dataSet che voglio analizzare
 startStamina,maxStamina,totalRounds,monsterToFace,monsterDesc,rewardList = parseInput("./2022/" + dataSetInTesting + ".txt")
 
 
-#Inizio algoritmo
+#Setting dei parametri globali (comuni per ogni iterazione)
 bestSolution = 0.0
 iteration = 0
 numIterazioni = 1000
@@ -23,55 +24,64 @@ numIterazioni = 1000
 
 while iteration<numIterazioni:
 
+    #Setting dei parametri locali (propri di ogni iterazione)
     stamina = np.full(totalRounds,startStamina)
     actualSolution = np.full(totalRounds, -1)
+    facedMonster = 0
 
+    #Simulo ciascun round di ogni iterazione
     for i in range(totalRounds):
 
-        #Trovo i mostri che posso affrontare
-        availableMonster = []
-        for j in range(monsterDesc.shape[0]):
-            if(monsterDesc[j][0] <= stamina[i]):
-                availableMonster.append(j)
-        
-        availableMonster = removeAlreadyDefeated(availableMonster, actualSolution)
+        if facedMonster <= monsterToFace:
+            #Trovo i mostri che posso affrontare
+            availableMonster = []
+            for j in range(monsterDesc.shape[0]):
+                if(monsterDesc[j][0] <= stamina[i]):
+                    availableMonster.append(j)
+            
+            availableMonster = removeAlreadyDefeated(availableMonster, actualSolution)
 
-        if(len(availableMonster)>0):
-            scores = rankFunction(availableMonster, monsterDesc, rewardList, totalRounds-i)
-            #print("Iteration {} Round {} Monsters {} scores: {}".format(iteration, i, availableMonster, scores))
+            if(len(availableMonster)>0):
+                #Calcolo la somma delle reward della lista degli availableMonster dal turno attuale fino alla fine
+                scores = rankFunction(availableMonster, rewardList, totalRounds-i)
+                #print("Iteration {} Round {} Monsters {} scores: {}".format(iteration, i, availableMonster, scores))
 
+                #DUE POSSIBILI ALTERNATIVE PER EFFETTUARE LA SCELTA DEL MOSTRO DA AFFRONTARE
 
-            #Scelta basata SOLO SULLA MASSIMA SOMMA DELLE REWARDS
-            #choice = availableMonster[np.argmax(scores)]
+                #Scelta basata SOLO SULLA MASSIMA SOMMA DELLE REWARDS
+                #choice = availableMonster[np.argmax(scores)]
 
-            #Scelta PROBABILISTICA BASATA SULLA MASSIMA SOMMA DELLE REWARDS
-            total = np.sum(scores)
-            if total != 0:
-                probabilities = [v/total for v in scores]
-                chosenRank = random.choices(scores, weights=probabilities)[0]
-                choice = availableMonster[scores.tolist().index(chosenRank)]
+                #Scelta PROBABILISTICA BASATA SULLA MASSIMA SOMMA DELLE REWARDS
+                total = np.sum(scores)
+                if total != 0:
+                    probabilities = [v/total for v in scores]
+                    chosenRank = random.choices(scores, weights=probabilities)[0]
+                    choice = availableMonster[scores.tolist().index(chosenRank)]
+                else:
+                    probabilities = np.ones(len(scores))/len(scores)
+                    chosenRank = random.choices(scores, weights=probabilities)[0]
+                    choice = availableMonster[scores.tolist().index(chosenRank)]
+
+                #Registro la scelta nella soluzione attuale
+                actualSolution[i] = choice
+                
+                #Rimuovo la stamina consumata e aggiungo quella guadagnata
+                stamina[i+1:] = stamina[i+1:] - monsterDesc[choice][0]
+                stamina[i+monsterDesc[choice][1]:] = stamina[i+monsterDesc[choice][1]:] + monsterDesc[choice][2]
+
+                stamina = checkStamina(stamina, maxStamina)
+                facedMonster += 1
+
             else:
-                probabilities = np.ones(len(scores))/len(scores)
-                chosenRank = random.choices(scores, weights=probabilities)[0]
-                choice = availableMonster[scores.tolist().index(chosenRank)]
+                actualSolution[i] = -1
 
-            
-            actualSolution[i] = choice
-            
-            #Rimuovo la stamina consumata e aggiungo quella guadagnata
-            stamina[i+1:] = stamina[i+1:] - monsterDesc[choice][0]
-            stamina[i+monsterDesc[choice][1]:] = stamina[i+monsterDesc[choice][1]:] + monsterDesc[choice][2]
-
-            stamina = checkStamina(stamina, maxStamina)
-
-        else:
-            actualSolution[i] = -1
-
+    #Calcolo il punteggio della soluzione attuale
     solutionScore = solutionFinalScore(actualSolution, rewardList)
     print("ITERAZIONE {} PUNTEGGIO SOLUZIONE: {}".format(iteration, solutionScore))
 
     if(solutionScore>bestSolution):
         bestSolution = solutionScore
+        #Viene salvata la soluzione migliore nel percorso ./2022/Solutions/$dataSetInTesting/soluzioneMigliore.txt
         saveSolution(actualSolution, iteration, solutionScore, "./2022/Solutions/" + dataSetInTesting + "/")
     
     iteration += 1
--- 2022/Utility.py
+++ 2022/Utility.py
@@ -1,30 +1,30 @@
 import os
 import numpy as np
 
-
-def rankFunction(availableMonster, monsterDesc, rewardList, remainingRounds):
+#Calcolo la somma delle reward della lista degli availableMonster dal turno attuale fino alla fine
+def rankFunction(availableMonster, rewardList, remainingRounds):
 
     rewardSumList = np.zeros(len(availableMonster))
     for i in range(len(availableMonster)):
         rewardSumList[i] = np.sum(rewardList[availableMonster[i]][:remainingRounds])
 
-    #LA SOMMA DELLE REWARD NON BASTA A DEFINIRE LO SCORE... DA COMPLETARE
-
-    
-
     return rewardSumList
 
-#La monsterList che riceve deve avere un mostro per turno, se in un turno non si è preso nessun mostro va messo un -1
+
+""" 
+Questa funzione calcola il valore della soluzione passata (monsterList).
+La monsterList che riceve deve avere un mostro per turno, se in un turno non si è preso nessun mostro va messo un -1 
+"""
 def solutionFinalScore(monsterList, rewardList):
     scorePerRound =  np.zeros(len(monsterList))
     for i in range(len(monsterList)):
         if(monsterList[i]>=0):
             scorePerRound[i:] += rewardList[monsterList[i]][:len(monsterList)-i]
 
-
     return np.sum(scorePerRound)
 
 
+#Salva la soluzione in un file di testo
 def saveSolution(solution, iteration, score, path):
 
     # apri il file in modalità scrittura (w)
@@ -42,6 +42,7 @@ def saveSolution(solution, iteration, score, path):
                 file.write(str(num) + "\n")
 
 
+#Vengono rimossi dalla lista degli availableMonster i mostri già affrontati in passato
 def removeAlreadyDefeated(availableMonster, actualSolution):
 
     finalAvailableMonster = availableMonster.copy()
@@ -52,6 +53,8 @@ def removeAlreadyDefeated(availableMonster, actualSolution):
     
     return finalAvailableMonster
 
+
+#Viene controllato che la stamina non ecceda il valore massimo
 def checkStamina(stamina, maxStamina):
 
     finalStamina = stamina.copy()
